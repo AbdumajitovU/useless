4d25f4dd850dfb41dd304ee31cb46db153083f25
bro-web <abdumajitovumirzoq@gmail.com>
2022-03-18 17:38:39 +0500

    useless commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import Layout from "../components/layout";
+import Sidebar from "../components/sidebar";
+
+export default function Index({ data }) {
+  return (
+    <section>
+      <h1>Stadionlar</h1>
+      {data.length > 0 &&
+        data.map((p) => {
+          return <p>{p.title}</p>;
+        })}
+    </section>
+  );
+}
+
+Index.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <Sidebar />
+      {page}
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
